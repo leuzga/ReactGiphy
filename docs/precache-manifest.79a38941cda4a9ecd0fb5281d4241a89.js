59471ad2bc5bc49099865d2c87ed6f5d2c75c574
Lenin Emiro Uzcategui Garcia <lenin.uzcategui@globant.com>
2021-05-17 18:02:52 -0400

    initial set giphy project

---
+++ docs/precache-manifest.79a38941cda4a9ecd0fb5281d4241a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a410773ff3052c7cc1f1af7fa6abf4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffcf87d46c66e317be1a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcf87d46c66e317be1a",
+    "url": "/static/js/main.d4581626.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
